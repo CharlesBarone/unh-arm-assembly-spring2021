37ba19f0a3376d33439d42ba82e84d4130692549
Charles_ <charliebarone2@icloud.com>
2021-03-25 01:12:54 -0400

    WEEK5: IT WORKS!!!

--- WEEK5_Barone/main.s
+++ WEEK5_Barone/main.s
@@ -6,101 +6,96 @@
 @	10 32 bit words.
 @
 @	FIFO Registers:
-@	r4 - Head Pointer
-@	r5 - Tail Pointer
+@	fifo - Head Pointer
+@	fifo + tailPtr - Tail Pointer
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-	
 	.global main
-	
+
 	.text
+@ Initializes fifo data structure
+fifoInit:
+	push	{lr}
+	mov	r0, #0
+	ldr	r3, =fifo
+	ldr	r4, =tailPtr
+	str	r0, [r4]
+	pop	{pc}
+
+@ Push a 32 bit word onto the end of the fifo
+@ Args:
+@ r0 - 32 bit word
+fifoPush:
+	push	{lr}
+	ldr	r3, =fifo		@ Get head pointer
+	ldr	r5, =tailPtr		@ Get tail pointer increment amount pointer
+	ldr	r2, [r5]		@ Get value of tail pointer increment
+	
+	cmp	r2, #12			@ If fifo is full skip push TODO: Change to 44 from 12
+	beq	noPush
+	str	r0, [r3, r2]		@ Store 32 bit word argument into headptr + tail increment amount
+	add	r2, #4			@ Increment tail pointer increment by 1 word
+	str	r2, [r5]		@ Store new tail pointer
+noPush:
+	pop	{pc}
+
+@ Pop a 32 bit word off the front of the fifo
+@ Returns:
+@ r0 = 32 bit word
+fifoPop:
+	push	{lr}
+	ldr	r3, =fifo		@ Get head pointer
+	ldr	r4, =tailPtr		@ Get tail pointer increment amount
+	ldr	r2, [r4]		@ Get value of tail pointer increment
+	
+	cmp	r2, #0			@ If fifo is empty, skip pop
+	beq	noPop
+
+	ldr	r0, [r3]		@ Get return value
+	push	{r0}			@ Push return value onto the stack
+	push	{r2}			@ Store current tail pointer increment on stack for later	
+
+	ldr	r5, [r3, r2]		@ Get value current end of fifo
+loop:
+	sub	r2, #4			@ Decrement tail pointer increment by 1 word
+	ldr	r6, [r3, r2]		@ Get value of new end of fifo
+	str	r5, [r3, r2]		@ Shift value 1 position to left in fifo
+	mov	r5, r6			@ Move r6 to r5 ( new value to be shifted)
+
+	cmp	r2, #0			@ If taill pointer increment == decimal number 0, end loop
+	beq	endLoop	
+
+	b	loop			@ Unconditional branch to loop
+
+endLoop:
+	pop	{r2}			@ Pop original tail pointer increment off stack
+	sub	r2, #4			@ Decrement tail pointer increment by 1 word
+	str	r2, [r4]		@ Restore tail ptr increment
+	pop	{r0}			@ Pop return value off the stack
+noPop:
+	pop	{pc}
+
+@ Exits the program gracefully
 terminate:
 	mov	r0, #0
 	mov	r7, #1
 	swi	0
 
-fifoInit:
-	push	{lr}
-	ldr	r4, =fifo	@ Head Pointer
-	ldr	r5, =fifo	@ Tail Pointer
-	pop	{pc}
-
-@ Push a 32 bit word onto the end of the queue.
-@ Args:
-@ r0 - 32 bit word
-fifoPush:
-	push	{lr}
-	add	r1, r4, #40	@ Get last pointer + 1
-	cmp	r5, r1		@ Check if queue is full
-	beq	noPush
-	add	r5, #4		@ Increment Tail Pointer by 4 bytes
-	str	r0, [r5]	@ Store r0 at Tail Pointer
-noPush:
-	pop	{pc}
-
-@ Pop a 32 bit word off the front of the queue
-@ Returns:
-@ r0 - 32 bit word
-fifoPop:
-	push	{lr}
-	bl	fifoState
-	cmp	r0, #0		@ If queue is empty
-	beq	skip		@ Nothing to remove from queue
-	
-	ldr	r0, [r4]	@ Get return value
-	push	{r0}
-	mov	r1, #4		@ Counter
-	mov	r4, r3		@ temp lead pointer
-loop:
-	cmp	r1, #40		@ Check if reached end of memory space
-	beq	endLoop
-	add	r2, r1, r4	@ Get temp pointer
-	ldr	r0, [r2]	@ Get value to move
-	str	r0, [r3]	@ Move value up in queue
-	mov	r3, r2		@ Get new temp lead pointer
-	add	r2, #4		@ Increment temp pointer 
-	add	r1, #4		@ Increment counter
-	b	loop		@ Unconditional branch to loop
-endLoop:
-	mov	r1, #0		@ Store #0 in r1
-	str	r1, [r4,#36]	@ Zero out newly opened queue slot
-skip:
-	pop	{r0}
-	pop	{pc}
-
-@ Returns 0 if queue is empty, 1 if it is not empty.
-@ Returns:
-@ r0 - #0 or #1
-fifoState:
-	push	{lr}
-	cmp	r4, r5		@ If tail pointer == head pointer
-	beq	true
-	mov	r0, #0
-	pop	{pc}
-true:
-	mov	r0, #1
-	pop	{pc}
-
+@ Program entry point
 main:
-	mov	r6, #0		@ Number to push/pop and increment
-mainLoop:
-	cmp	r6, #10		@ If r6 == 10
-	beq	finish
-	mov	r0, r6		@ Store number into r0 to be pushed to queue
+	bl	fifoInit
+	mov	r0, #4
 	bl	fifoPush
-	add	r6, #1		@ r6++
-	b	mainLoop
-finish:
-	mov	r6, #0
-mainLoop2:
-	cmp	r6, #10		@ If r6 == 10
-	beq	finish2
+	mov	r0, #3
+	bl	fifoPush
 	bl	fifoPop
-	add	r6, #1		@ r6++
-	b	mainLoop2
-finish2:
+	bl	fifoPop
 	b	terminate
+
 
 	.data
 fifo:
-	.space	40	
+	.space	40
 	.equ	fifoLen, (.-fifo)
+
+tailPtr:
+	.space 8
