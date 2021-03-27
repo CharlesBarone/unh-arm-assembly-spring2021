e7d5dbb2a212629fb1972fccfd199c807657cd3d
Charles_ <charliebarone2@icloud.com>
2021-03-27 02:55:01 -0400

    WEEK6: Initial commit, may or may not actually work...

--- WEEK6_Barone/main.s
+++ WEEK6_Barone/main.s
@@ -1,20 +1,128 @@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @
 @	Assembly Week 6 Program
-@	Charles Barone		3/16/2021
-@	Implements a ring buffer.
+@	Charles Barone		3/27/2021
+@	Implements a Ring Buffer  Data Structure capable of
+@	storing 10 32 bit words.
 @
-@
+@	Buffer Registers:
+@	writePtr - Write Pointer
+@	readPtr - Read Pointer
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
 	.global main
 
 	.text
+@ Initializes ring buffer data structure
+ringInit:
+	push	{lr}
+	mov	r0, #0
+	ldr	r3, =readPtr
+	ldr	r4, =writePtr
+	str	r0, [r4]
+	str	r0, [r3]
+	pop	{pc}
+
+@ Exits the program gracefully
 terminate:
-	move	r0, #0
+	mov	r0, #0
 	mov	r7, #1
 	swi	0
 
+@ Computes mod N of a number
+@ Args:
+@ r0 - number
+@ r1 - mod
+@ Returns:
+@ r0 - remainder
+@ r1 - N
+modN:
+	push	{lr}
+	mov	r3, #0
+cont:
+	sub	r0, r1
+	add	r3, #1
+	cmp	r0, #0
+	bgt	cont
+	cmp	r0, #0
+	beq	modDone
+	add	r0, r1
+	sub	r3, #1
+modDone:
+	mov	r1, r3
+	pop	{pc}
+
+@ Writes 1 32 bit word to ring buffer
+@ Args:
+@ r0 - 32 bit word
+bufWrite:
+	push	{lr}
+	ldr	r5, =readPtr	@ Get read pointer
+	ldr	r4, [r5]	@ Get read pointer increment amount
+	ldr	r5, =writePtr	@ Get write pointer
+	ldr	r3, [r5]	@ Get write pointer increment amount
+	bl	incrementPtr	@ Increment write pointer by 1 word
+	cmp	r3, r4		@ Check if buffer is full
+	beq	noWrite
+
+	ldr	r3, [r5]	@ Get original write pointer increment amount
+	ldr	r1, =ring	@ Get ring buffer
+	str	r0, [r1, r3]	@ Write r0 to buffer
+	bl	incrementPtr	@ Increment write pointer
+	str	r3, [r5]	@ Store new write pointer
+noWrite:
+	pop	{pc}
+
+@ Increments a pointer offset by 1 with 
+@ respect to the circular Data Structure
+@ Args:
+@ r3 - pointer
+@ Returns:
+@ r3 - new pointer
+incrementPtr:
+	push	{lr}
+	push	{r0-r1}
+	ldr	r1, =#40	@ Store length of buffer in r1
+	mov	r0, r3		@ Move pointer into r0
+	add	r0, #4		@ Increment pointer by 1 word
+	bl	modN		@ Run modN
+	mov	r3, r1		@ Move new pointer back to r3
+	pop	{r0-r1}
+	pop	{pc}
+
+
+@ Reads 1 32 bit word from the ring buffer
+@ Returns:
+@ r0 - 32 bit word
+bufRead:
+	push	{lr}
+	ldr	r5, =writePtr	@ Get write pointer
+	ldr	r4, [r5]	@ Get write pointer increment amount
+	ldr	r5, =readPtr	@ Get read pointer
+	ldr	r3, [r5]	@ Get read pointer increment amount
+	cmp	r3, r4		@ Check if buffer is empty
+	beq	noRead
+
+	ldr	r1, =ring	@ Get ring buffer
+	ldr	r0, [r1, r3]	@ Read 1 word from buffer into r0
+	bl	incrementPtr	@ Increment read pointer by 1 word
+	str	r3, [r5]
+noRead:
+	pop	{pc}
+
+@ Program entry point
 main:
-	
-	b terminate
+	bl	ringInit
+
+
+	b	terminate
+
+	.data
+ring:
+	.space	40
+	.equ	ringLen, (.-fifo)
+
+writePtr:
+	.space 8
+
+readPtr:
+	.space 8
--- WEEK7_Barone/main.s
+++ WEEK7_Barone/main.s
@@ -1,20 +1,87 @@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @
 @	Assembly Week 7 Program
-@	Charles Barone		3/16/2021
-@	Implements a data "parser".
+@	Charles Barone		3/25/2021
+@	Implements a command parser.
 @
 @
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 	.global main
-
+	.equ	STDIN, 0
+	.equ	STDOUT, 1
 	.text
+prompt:
+	.asciz "Please enter a command >>"
+
+	.align
 terminate:
 	move	r0, #0
 	mov	r7, #1
 	swi	0
 
-main:
+@ Function to take user input
+@ Returns:
+@ RxBuff - User input in ascii
+uio:
+	push	{lr}
+	ldr	r0, =prompt
+	bl	puts
+	mov	r0, #STDIN
+	ldr	r1, =RxBuff
+	mov	r2, #RxBuffLen
+	bl	read
+	pop	{pc}
+
+@ Function to parse user input
+@ Args:
+@ RxBuff - User input in ascii
+parser:
+	push	{lr}
+	mov	r8, #0			@ Counter
+	ldr	r5, =RxBuff		@ Load user input ascii into r5
+	ldrb	r1, [r5, r8]		@ Get first ascii character
+	add	r8, #1			@ Increment Counter
+	cmp	r1, #0x41		@ if command == A
+	beq	A
+	cmp	r1, #0x42		@ if command == B
+	beq	B
+	b	invalid			@ Unconditional branch to invalid, b/c no valid command
+A:
+	ldrb	r1, [r5. r8]		@ Verify size == 0
+	cmp	r1, #0x30
+	bne	invalid
+	add	r8, #1			@ Increment Counter
+	ldrb	r1, [r5, r8]		@ Verify CR
+	cmp	r1, 
+	b	skipB
+B:
+
+
+skipB:
+
+
+	b	valid
+invalid:
 	
-	b terminate
+	b	end
+valid:
+	
+	
+	pop	{pc}
+
+main:
+	bl	uio
+	bl	parser
+	b	terminate
+
+	.data
+TxBuff:
+	.space	6
+	.equ	TxBuffLen, (.-TxBuff)
+
+RxBuff:
+	.space	256
+	.equ	RxBuffLen, (.-RxBuff)
+
+
